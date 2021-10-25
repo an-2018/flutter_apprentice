fd0001ef37ee5a67e91c926e2cc1e2fd2a43bf8a
anilson <anilsonmonteiro@gmail.com>
2021-10-25 19:12:17 -0100

    conplete secciton 2 project

--- secction_2_everythings_widget/fooderlich/lib/home.dart
+++ secction_2_everythings_widget/fooderlich/lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
 
+import 'card3.dart';
 import 'fooderlich_theme.dart';
 
 class Home extends StatefulWidget {
@@ -13,9 +16,9 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    Container(color: Colors.red,),
-    Container(color: Colors.green,),
-    Container(color: Colors.blue,),
+    const Card1(),
+    const Card2(),
+    const Card3(),
   ];
 
   void _onItemTapped(int index) {
@@ -23,27 +26,36 @@ class _HomeState extends State<Home> {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO: Style the title
       appBar: AppBar(
         title: Text(
           'Fooderlich',
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
-      // TODO: Style the body text
       body: pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _onItemTapped,
         currentIndex: _selectedIndex,
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
         items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Card"),
-          const BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Card2"),
-          const BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Card3"),
-      ],),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card2",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: "Card3",
+          ),
+        ],
+      ),
     );
   }
 }
